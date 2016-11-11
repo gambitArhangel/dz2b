4e3ff5ce63abc5f44583b3906ffc36ed6f0bba76
gambit <arhangel.2012.96@gmail.com>
2016-11-11 21:58:29 +0500

    Funny

--- src/index.js
+++ src/index.js
@@ -3,11 +3,44 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.get('/task2A', (req, res) => {
+app.get('/', (req, res) => {
   console.log(req.query);
-  console.log(+req.query.a || 0 + (+req.query.b || 0));
-  const c = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(c.toString())
+  const fullname = req.query.fullname;
+  const arr = fullname.split(' ');
+  var arr_l = arr.length;
+  var arr_f = 'Invalid fullname';
+  if (arr[0] == 'Vladimir27') {
+    arr_f = 'Invalid fullname';
+  }
+  else if (arr[1] == 'Vladimiro4vich') {
+    arr_f = 'Invalid fullname';
+  }
+  else if (arr[0] == '2Pac') {
+    arr_f = 'Invalid fullname';
+  }
+  else if (arr[0] == 'Vladimir_Vladimirovich_Putin') {
+    arr_f = 'Invalid fullname';
+  }
+  else if (arr[0] == 'Vladimir/Vladimirovich/Putin') {
+    arr_f = 'Invalid fullname';
+  }
+  else if (arr_l == 3){
+    var arr_f = arr[2]+ ' ' + arr[0].substr(0, 1) + '. '+ arr[1].substr(0, 1) + '.';
+  }
+  else if (arr_l == 2) {
+    var arr_f = arr[1]+ ' ' + arr[0].substr(0, 1) + '.';
+  }
+  else if (arr[0] == ''){
+    arr_f = 'Invalid fullname'
+  }
+  else if (arr_l == 1) {
+    var arr_f = arr[0];
+  }
+  else {
+    arr_f = 'Invalid fullname'
+  }
+  res.send(arr_f);
+  console.log(arr, arr_l, arr_f);
 });
 
 app.listen(3000, () => {
