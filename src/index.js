46e5ecd37456e9c3348465199fad991151194d77
gambit <arhangel.2012.96@gmail.com>
2016-11-11 19:59:21 +0500

    Finish

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+app.get('/task2A', (req, res) => {
+  console.log(req.query);
+  console.log(+req.query.a || 0 + (+req.query.b || 0));
+  const c = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(c.toString())
 });
 
 app.listen(3000, () => {
